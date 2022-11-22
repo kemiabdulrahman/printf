7a405bd8121f018caa790b94b5bbdd1299ffdce7
EmmylsonTech <ehis.emmylson@gmail.com>
2022-11-22 04:40:41 -0800

    all ev and fill file

--- fill_long_oct_array.c
+++ fill_long_oct_array.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * fill_oct_array - writes the character c to stdout
+ * fill_long_oct_array - calculates a long octal number
  *
  * @bnr: array where is stored the binary.
  * @oct: array where is stored the octal.
  *
  * Return: binary array.
  */
-char *fill_oct_array(char *bnr, char *oct)
+char *fill_long_oct_array(char *bnr, char *oct)
 {
 	int op, i, j, ioct, limit;
 
-	oct[11] = '\0';
-	for (i = 31, ioct = 10; i >= 0; i--, ioct--)
+	oct[22] = '\0';
+	for (i = 63, ioct = 21; i >= 0; i--, ioct--)
 	{
-		if (i > 1)
+		if (i > 0)
 			limit = 4;
 		else
-			limit = 2;
+			limit = 1;
 		for (op = 0, j = 1; j <= limit; j *= 2, i--)
 			op = ((bnr[i] - '0') * j) + op;
 		i++;
